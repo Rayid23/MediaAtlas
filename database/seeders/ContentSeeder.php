f07362f4aefae4b3a8932942fbe3cff1cd7dbe3f
Rayid32 <madiyorshodiev333@gmail.com>
2025-05-08 20:12:52 +0500

    MINOR: Added home-view and added font resource

--- database/seeders/ContentSeeder.php
+++ database/seeders/ContentSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Content;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class ContentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Content::factory()->count(10)->create();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    dd(123);
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
